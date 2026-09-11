c8c5d4c7198ce35da66d961d5164d0019df51d21
samad-source <s.azeez8553@miva.edu.ng>
2026-09-11 12:09:18 +0100

    week1

---
+++ week1_practice.R
@@ -0,0 +1,42 @@
+# Basic Arithmetic
+6 + 5
+7 - 3
+8 * 4
+8 / 4
+8 ^ 4
+
+#Quotient and Remainder
+8 %/% 5
+8 %% 5
+
+# Pi
+pi
+
+# Rounding
+round(3.14159265, 3)
+ceiling(4.2)
+floor(4.8)
+
+# Integer conversion
+as.integer(9.87)
+
+# Sequences
+1:10
+seq(from = 0, to = 20, by = 5)
+
+# Repetition
+rep(10, 5)
+
+# Sampling
+sample(1:20, 5)
+
+# Logical comparisons
+10 > 5
+10 < 5
+10 == 10
+10 != 5
+
+# Logical functions
+all(c(TRUE, TRUE, TRUE))
+any(c(FALSE, FALSE, TRUE))
+
